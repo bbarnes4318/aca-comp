21b3e7895321475042b3748059329caee4ec338c
Braden Barnes <bbarnes4318@gmail.com>
2025-10-17 13:55:35 -0400

    Add React app structure for Digital Ocean deployment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ACACalculator from './comp.js';
+
+function App() {
+  return (
+    <div className="App">
+      <ACACalculator />
+    </div>
+  );
+}
+
+export default App;
